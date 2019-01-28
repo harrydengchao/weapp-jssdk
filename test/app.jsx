5b4fd7e9f0280e5b8a5fee1f0efb96c7805c36a8
LAPTOP-4HR5MNDJ\邓超 <dengchao2056@126.com>
2019-01-28 17:04:46 +0800

    填坑

--- test/app.jsx
+++ test/app.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import VConsole from 'vconsole'
+import axios from 'axios'
 import { 
   WXSDK,
-  QQSDK
+  WBSDK
 } from '@lib/index'
 
 new VConsole()
@@ -10,42 +11,73 @@ const UA = window.navigator.userAgent
 
 export default class Index extends Component {
   componentDidMount() {
+
+  }
+
+  weixinAuth() {
     if (/MicroMessenger/i.test(UA)) {
-      let instance = new WXSDK({
+      let signatureServer = 'https://bcs.link-us.com.cn/wechat_sit/frontend/directSelling/directSellingBank/weixinConfig'
+      let instance = new WXSDK(signatureServer, {
         debug: true
-      }, {
-        signatureServer: 'http://bcs.link-us.com.cn/wechat_sit/frontend/directSelling/directSellingBank/weixinConfig'
       })
-      instance.ready({
+      instance.share({
         title: '1231',
         desc: 'sdfsd',
+        imgUrl: 'http://bcs.link-us.com.cn/act_uat/static/shareImg.jpg?timestamp=1548567423885',
         success: () => {
           console.log('success..')
         }
       })
-    } else if (/\s+QQ\//i.test(UA)) {
-      let instance = new QQSDK()
-      instance.ready({
+    }
+  }
+
+  handleWeixinScope() {
+    axios
+      .get('http://bcs.link-us.com.cn/wechat_sit/frontend/wechatAjaxRedirect')
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  weiboAuth() {
+    if (/Weibo/i.test(UA)) {
+      this.instance = new WBSDK()
+      this.instance.share({
         title: '1231',
         desc: 'sdfsd',
-        success: () => {
-          console.log('success..')
-        }
+        imgUrl: 'http://bcs.link-us.com.cn/act_uat/static/shareImg.jpg?timestamp=1548567423885'
       })
-    } else if (/\s+MQQBrowser\//i.test(UA)) {
+    }
+  }
 
-    } else if (/Weibo/i.test(UA)) {
+  qqAuth() {
+    if (/\s+QQ\//i.test(UA)) {
 
-    } else if (/\S+UCBrowser\//i.test(UA)) {
+    }
+  }
+
+  ucbrowserAuth() {
+    if (/\S+UCBrowser\//i.test(UA)) {
       
     }
   }
 
   render() {
     return (
-      <div>
-        asdfasd
-      </div>
+      <React.Fragment>
+        <div>
+          <h3>调试微信</h3>
+          <p><button onClick={this.weixinAuth.bind(this)}>微信 jssdk 签名</button></p>
+          <div><button onClick={this.handleWeixinScope.bind(this)}>微信网页授权</button></div>
+        </div>
+        <div>
+          <h3>调试微博</h3>
+          <p><button>调用微博分享</button></p>
+        </div>
+      </React.Fragment>
     )
   }
 }
